344f03216fab06c2d8ea789e6b8d4f72831a7e1d
mmema <marin_mema@sfu.ca>
2020-06-22 05:25:59 -0700

    Use tile images instead of canvas drawing

--- browser/minesweeper-canvas.js
+++ browser/minesweeper-canvas.js
@@ -6,10 +6,30 @@ const CHECKER_LIGHT = '#ffffff';
 const DISABLED_HUE = 'rgba(200, 200, 200, 0.5)';
 const OUTLINE_COLR = '#ffa500';
 
+function preloaded(src) {
+  const img = new Image();
+  img.src = src;
+  return img;
+}
+
 // Flag icon made by Freepik from www.flaticon.com
 // https://www.flaticon.com/free-icon/flag_94182
-const PLAYER_FLAGS_IMG = ['player_flag_0.svg', 'player_flag_1.svg'];
-
+const TILE_IMAGES = {
+  'CHECKER-DARK':   preloaded('tiles/checker-dark.png'),
+  'CHECKER-LIGHT':  preloaded('tiles/checker-light.png'),
+  'OUTLINE':        preloaded('tiles/outline.png'),
+  'A':              preloaded('tiles/A.png'),
+  'B':              preloaded('tiles/B.png'),
+  '0':              preloaded('tiles/0.png'),
+  '1':              preloaded('tiles/1.png'),
+  '2':              preloaded('tiles/2.png'),
+  '3':              preloaded('tiles/3.png'),
+  '4':              preloaded('tiles/4.png'),
+  '5':              preloaded('tiles/5.png'),
+  '6':              preloaded('tiles/6.png'),
+  '7':              preloaded('tiles/7.png'),
+  '8':              preloaded('tiles/8.png'),
+};
 
 
 // Turn a <canvas> into a tiled N-by-M board.
@@ -81,69 +101,31 @@ function CanvasTiles(N, M, W, H, onclick) {
 
     var layers = new Array();
 
-    function stackdraw(name, f) {
-      return function(args) {
-        layers.push({name, f, args});
-        f(args);
-      }
+    function render(img) {
+      surface.drawImage(img, x, y);
     }
 
     function redraw() {
       layers.forEach(function(item) {
-        item.f(item.args);
+        render(item.src);
       });
-    }
-
-    function eraselayer(name) {
-      layers = layers.filter((item) => item.name != name);
-      redraw();
     }
 
     tiles[i * M + j] = {
       hidden: true,
       value: null,
-      fill:
-      stackdraw('fill', function(args) {
-        surface.fillStyle = args.colour;
-        surface.fillRect(x, y, W, H);
-      }),
 
-      outline:
-      stackdraw('outline', function(args) {
-        surface.strokeStyle = args.colour;
-        surface.lineWidth = 2;
-        surface.strokeRect(x + 1, y + 1, W - 2, H - 2)
-      }),
-
-      text:
-      stackdraw('text', function(args) {
-        const font_size = Math.floor(0.7 * W);
-    
-        surface.fillStyle = args.colour;
-        surface.font = 'bold ' + font_size + 'px arial';
-        surface.textBaseline = 'top';
-        const metrics = surface.measureText(args.str);
-        const ch_width = metrics.width;
-        const ch_height = metrics.actualBoundingBoxDescent;
-    
-        const text_x = x + (0.5 * (W - ch_width));
-        const text_y = y + (0.5 * (H - ch_height));
-        surface.fillText(args.str, text_x, text_y);
-      }),
-
-      renderimage:
-      stackdraw('renderimage', function(args) {
-        var img = new Image();
-        img.onload = function() {
-          const img_x = x + (0.5 * (W - img.width));
-          const img_y = y + (0.5 * (H - img.height));
-          surface.drawImage(img, img_x, img_y);
-        }
-        img.src = args.src;
-      }),
+      draw:
+      function(name, src) {
+        layers.push({name, src});
+        render(src);
+      },
 
       erase:
-      eraselayer,
+      function(name) {
+        layers = layers.filter((item) => item.name != name);
+        redraw();
+      }
     };
   });
 
@@ -182,44 +164,26 @@ function MinesweeperBoard(N, M, S, onclick) {
       // odd row & odd col  or  even row & even col
       const ee = (i % 2 === 0) && (j % 2 === 0); // even/even
       const oo = (i % 2 === 1) && (j % 2 === 1); // odd/odd
-      const colour = (ee || oo)? CHECKER_DARK : CHECKER_LIGHT;
-      board.tile(i, j).fill({colour});
+      const tileimg = (ee || oo)? 'CHECKER-DARK' : 'CHECKER-LIGHT';
+      board.tile(i, j).draw('checker', TILE_IMAGES[tileimg]);
     });
     board.enable();
   }
 
   board.setvalue = function(i, j, value) {
-    var colour = null;
-    switch (value) {
-      case 0: colour = '#eeeeee';  break;
-      case 1: colour = '#0000ff';  break;
-      case 2: colour = '#107118';  break;
-      case 3: colour = '#ff00ff';  break;
-      case 4: colour = '#ff0000';  break;
-      case 5: colour = '#ff0000';  break;
-      case 6: colour = '#ff0000';  break;
-      case 7: colour = '#ff0000';  break;
-      case 8: colour = '#ff0000';  break;
-      default: colour = '#ff0000';  break;
-    }
-
-    const str = value;
 
     board.tile(i,j).hidden = false;
     board.tile(i,j).value = value;
 
-    switch (value) {
-      case 'A':  board.tile(i,j).renderimage({src: PLAYER_FLAGS_IMG[0]}); break;
-      case 'B':  board.tile(i,j).renderimage({src: PLAYER_FLAGS_IMG[1]}); break;
-      default:   board.tile(i,j).text({str, colour});
-    }
+    // Assume valid value
+    board.tile(i,j).draw('value', TILE_IMAGES[String(value)]);
   };
 
   const lastselect = {i: 0, j: 0};
 
   board.select = function(i, j) {
     board.tile(lastselect.i, lastselect.j).erase('outline');
-    board.tile(i, j).outline({colour: OUTLINE_COLR});
+    board.tile(i, j).draw('outline', TILE_IMAGES['OUTLINE']);
     lastselect.i = i;
     lastselect.j = j;
   };
