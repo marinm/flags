193e3e2180541bd6d628f8ae00e3d8b47768ee04
mmema <marin_mema@sfu.ca>
2020-06-26 02:12:54 -0700

    Change style of score box

--- browser/minesweeper-events.js
+++ browser/minesweeper-events.js
@@ -171,6 +171,9 @@ const handlers = {
   start:
   function(msg) {
     room.start();
+
+    $('#player-0-score-box').addClass('active-turn');
+    $('#turn-score-container').removeClass('not-playing');
   },
 
   'opponent-disconnected':
