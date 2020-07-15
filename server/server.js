a98ea48077e137188feb8dc2815cb8df0480c405
mmema <marin_mema@sfu.ca>
2020-07-15 02:36:39 -0700

    Server reads WEBSITE_PORT from .env

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
+const dotenv = require('dotenv').config()
 const express = require('express')
 const app = express()
 const minesweeper = require('./url-router.js')
 
 app.use('/minesweeper', minesweeper)
-app.listen(8888)
+app.listen(process.env.WEBSITE_PORT)
