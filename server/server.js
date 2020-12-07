c9d097fa6da2d7594e27dc1f18ef56ad79fa0b8b
mmema <mmema@sfu.ca>
2020-12-06 22:17:23 -0500

    Remove static hosting

--- server/server.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const app = express()
-const minesweeper = require('./url-router.js')
-
-require('dotenv').config()
-
-app.use('/minesweeper', minesweeper)
-app.listen(process.env.WEBSITE_PORT)
--- server/url-router.js
+++
@@ -1,9 +0,0 @@
-// Serve static files
-
-const express = require('express')
-const path = require('path')
-const router = express.Router()
-
-router.use('/', express.static(path.join(__dirname, '../browser')))
-
-module.exports = router
