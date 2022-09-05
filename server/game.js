b3ae7074c73b86a5a088015f0cbe5eb869620e3c
marinm <marinmema@live.ca>
2022-09-05 00:22:35 -0400

    Use 4 spaces for indentation 🌶

--- server/game.js
+++ server/game.js
@@ -27,159 +27,159 @@ const labels = require('./labels.js');
 const randomBoard = require('./random-board.js');
 
 function FlagsGame(N, M, R) {
-  // A randomly generated board
-  const board = new randomBoard(N, M, R);
+    // A randomly generated board
+    const board = new randomBoard(N, M, R);
 
-  var turn = 0;
-  var score = [0, 0];
-  var seq = 0;
-  var on = true;
+    var turn = 0;
+    var score = [0, 0];
+    var seq = 0;
+    var on = true;
 
-  // The game is over when a player finds R/2 flags
-  const winning_score = Math.ceil(R/2);
+    // The game is over when a player finds R/2 flags
+    const winning_score = Math.ceil(R/2);
 
-  // What has been revealed so far (array of bools)
-  const revealed = new Array(N * M);
-  revealed.fill(false);
+    // What has been revealed so far (array of bools)
+    const revealed = new Array(N * M);
+    revealed.fill(false);
 
-  function get(i, j) {
-    const k = i * M + j;
-    if (!board.contains(i,j) || !revealed[k])
-      return null;
-    return board.at(i,j);
-  }
-
-  function reveal(i, j) {
-    const k = i * M + j;
-    revealed[k] = true;
-    return {i, j, value: board.at(i,j)};
-  }
-
-  // Reveal all remaining hidden tiles
-  // Should only be called when game is over
-  function revealall() {
-    if (on) {
-      return null;
+    function get(i, j) {
+        const k = i * M + j;
+        if (!board.contains(i,j) || !revealed[k])
+            return null;
+        return board.at(i,j);
     }
 
-    const remaining = [];
-    for (var i = 0; i < N; i++) {
-      for (var j = 0; j < M; j++) {
-        // A failed get() indicates an unrevealed tile
-        if (get(i,j) === null) {
-          remaining.push( reveal(i, j) );
-        }
-      }
-    }
-    return remaining;
-  }
-
-  function zerowalk(i, j) {
-    // The first zero must still be not revealed
-
-    // Stepped out of bounds
-    // Nothing to do
-    if (!board.contains(i,j))
-      return [];
-
-    const k = i * M + j;
-
-    // Stepped to a value that's already been revealed
-    // Nothing to do
-    if (revealed[k])
-      return [];
-
-    const value = board.at(i,j);
-
-    // Stepped to a flag
-    // Don't reveal it
-    if (value === labels.HIDDEN_FLAG)
-      return [];
-
-    // Stepped to a non-zero numeric tile
-    // Reveal it and return the value
-    if (value != '0')
-      return [ reveal(i, j) ];
-
-    // Found a zero...
-    // First item on the newly revealed array is this tile
-    var newrev = [ reveal(i, j) ];
-
-    function stepto(next_i, next_j) {
-      newrev = newrev.concat( zerowalk(next_i, next_j) );
-    }
-
-    stepto(i - 1, j - 1); // TL
-    stepto(i - 1, j - 0); // TC
-    stepto(i - 1, j + 1); // TR
-    stepto(i - 0, j - 1); // CL
-    stepto(i - 0, j + 1); // CR
-    stepto(i + 1, j - 1); // BL
-    stepto(i + 1, j - 0); // BC
-    stepto(i + 1, j + 1); // BR
-
-    return newrev;
-  }
-
-  function select(i, j) {
-    // Selected coordinates out of bounds
-    if (!board.contains(i,j))
-      return null;
-    
-    // The game is already over
-    if (!on)
-      return null;
-
-    const k = i * M + j;
-    var value = board.at(i,j);
-
-    // Which tiles to reveal, if any
-    var show = [];
-
-    // If the tile is already revealed, do nothing
-    if (!revealed[k]) {
-      seq++;
-
-      if (value === 0) {
-        turn = (turn + 1) % 2;
-        // The first zero must still be not revealed
-        show = zerowalk(i, j);
-      }
-      else {
-        // Revealed a non-zero value...
+    function reveal(i, j) {
+        const k = i * M + j;
         revealed[k] = true;
-
-        if (value === labels.HIDDEN_FLAG) {
-          // Change the tile value from HIDDEN_FLAG to PLAYER_FLAG
-          board.set(i, j, labels.PLAYER_FLAGS[turn]);
-          value = board.at(i,j);
-
-          // Still the same player's turn
-          score[turn]++;
-
-          on = (score[turn] < winning_score);
-        }
-        else {
-          turn = (turn + 1) % 2;
-        }
-        show = [{i, j, value}];
-      }
-
-      // The game is over on this move
-      // Append all unrevealed values
-      if (!on) {
-        show = show.concat( revealall() );
-      }
+        return {i, j, value: board.at(i,j)};
     }
 
-    return { show, turn, score, on };
-  }
+    // Reveal all remaining hidden tiles
+    // Should only be called when game is over
+    function revealall() {
+        if (on) {
+            return null;
+        }
 
-  function getstate() {
-    return { turn, score, seq, on };
-  }
+        const remaining = [];
+        for (var i = 0; i < N; i++) {
+            for (var j = 0; j < M; j++) {
+                // A failed get() indicates an unrevealed tile
+                if (get(i,j) === null) {
+                    remaining.push( reveal(i, j) );
+                }
+            }
+        }
+        return remaining;
+    }
 
-  return { N, M, R, board, get, select, getstate };
+    function zerowalk(i, j) {
+        // The first zero must still be not revealed
+
+        // Stepped out of bounds
+        // Nothing to do
+        if (!board.contains(i,j))
+            return [];
+
+        const k = i * M + j;
+
+        // Stepped to a value that's already been revealed
+        // Nothing to do
+        if (revealed[k])
+            return [];
+
+        const value = board.at(i,j);
+
+        // Stepped to a flag
+        // Don't reveal it
+        if (value === labels.HIDDEN_FLAG)
+            return [];
+
+        // Stepped to a non-zero numeric tile
+        // Reveal it and return the value
+        if (value != '0')
+            return [ reveal(i, j) ];
+
+        // Found a zero...
+        // First item on the newly revealed array is this tile
+        var newrev = [ reveal(i, j) ];
+
+        function stepto(next_i, next_j) {
+            newrev = newrev.concat( zerowalk(next_i, next_j) );
+        }
+
+        stepto(i - 1, j - 1); // TL
+        stepto(i - 1, j - 0); // TC
+        stepto(i - 1, j + 1); // TR
+        stepto(i - 0, j - 1); // CL
+        stepto(i - 0, j + 1); // CR
+        stepto(i + 1, j - 1); // BL
+        stepto(i + 1, j - 0); // BC
+        stepto(i + 1, j + 1); // BR
+
+        return newrev;
+    }
+
+    function select(i, j) {
+        // Selected coordinates out of bounds
+        if (!board.contains(i,j))
+            return null;
+
+        // The game is already over
+        if (!on)
+            return null;
+
+        const k = i * M + j;
+        var value = board.at(i,j);
+
+        // Which tiles to reveal, if any
+        var show = [];
+
+        // If the tile is already revealed, do nothing
+        if (!revealed[k]) {
+            seq++;
+
+            if (value === 0) {
+                turn = (turn + 1) % 2;
+                // The first zero must still be not revealed
+                show = zerowalk(i, j);
+            }
+            else {
+                // Revealed a non-zero value...
+                revealed[k] = true;
+
+                if (value === labels.HIDDEN_FLAG) {
+                    // Change the tile value from HIDDEN_FLAG to PLAYER_FLAG
+                    board.set(i, j, labels.PLAYER_FLAGS[turn]);
+                    value = board.at(i,j);
+
+                    // Still the same player's turn
+                    score[turn]++;
+
+                    on = (score[turn] < winning_score);
+                }
+                else {
+                    turn = (turn + 1) % 2;
+                }
+                show = [{i, j, value}];
+            }
+
+            // The game is over on this move
+            // Append all unrevealed values
+            if (!on) {
+                show = show.concat( revealall() );
+            }
+        }
+
+        return { show, turn, score, on };
+    }
+
+    function getstate() {
+        return { turn, score, seq, on };
+    }
+
+    return { N, M, R, board, get, select, getstate };
 }
 
 module.exports = FlagsGame;
--- server/main.js
+++ server/main.js
@@ -21,8 +21,8 @@ var PLAYER_A = null;
 var PLAYER_B = null;
 
 const server = https.createServer({
-  cert: fs.readFileSync(process.env.SSL_CERT),
-  key: fs.readFileSync(process.env.SSL_KEY)
+    cert: fs.readFileSync(process.env.SSL_CERT),
+    key: fs.readFileSync(process.env.SSL_KEY)
 });
 const wss = new WebSocket.Server({ server });
 wss.on('connection', new_session);
@@ -33,17 +33,17 @@ console.log('Secure WebSocket Server started');
 console.log(`Listening for clients on ${SERVER_PORT}...`);
 
 function heartbeat() {
-  this.isAlive = true;
+    this.isAlive = true;
 }
 
 function ping() {
-  wss.clients.forEach(function each(ws) {
-    if (ws.isAlive === false)
-      return ws.terminate();
- 
-    ws.isAlive = false;
-    ws.ping(function noop(){});
-  });
+    wss.clients.forEach(function each(ws) {
+        if (ws.isAlive === false)
+            return ws.terminate();
+
+        ws.isAlive = false;
+        ws.ping(function noop(){});
+    });
 }
 
 // Every 30 seconds, ping all clients
@@ -63,81 +63,81 @@ function new_session(ws) {
 
     console.log('new connection');
 
-  ws.isAlive = true;
-  ws.on('pong', heartbeat);
+    ws.isAlive = true;
+    ws.on('pong', heartbeat);
 
-  // Received message from client
-	ws.on('message', function(data) {
-    receive(ws, parse_json(data));
-  });
+    // Received message from client
+    ws.on('message', function(data) {
+        receive(ws, parse_json(data));
+    });
 
-  // Client connection closed
-  ws.on('close', function() {
-    close(ws);
-  });
+    // Client connection closed
+    ws.on('close', function() {
+        close(ws);
+    });
 
-  if (PLAYER_A === null) {
-    PLAYER_A = ws;
-    ws.send(JSON.stringify({ type: 'join', status: 'OPEN', playing_as: 0 }));
-  }
-  else if (PLAYER_B === null) {
-    PLAYER_B = ws;
-    ws.send(JSON.stringify({ type: 'join', status: 'OPEN', playing_as: 1 }));
+    if (PLAYER_A === null) {
+        PLAYER_A = ws;
+        ws.send(JSON.stringify({ type: 'join', status: 'OPEN', playing_as: 0 }));
+    }
+    else if (PLAYER_B === null) {
+        PLAYER_B = ws;
+        ws.send(JSON.stringify({ type: 'join', status: 'OPEN', playing_as: 1 }));
 
-    PLAYER_A.send(JSON.stringify({ type: 'start' }));
-    PLAYER_B.send(JSON.stringify({ type: 'start' }));
-  }
-  else {
-    ws.send(JSON.stringify({ type: 'join', status: 'BUSY' }));
-  }
+        PLAYER_A.send(JSON.stringify({ type: 'start' }));
+        PLAYER_B.send(JSON.stringify({ type: 'start' }));
+    }
+    else {
+        ws.send(JSON.stringify({ type: 'join', status: 'BUSY' }));
+    }
 }
 
 function close(ws) {
-  // On disconnect, 
-  // [1] Client was PLAYING_AS 'A' and waiting for 'B', end game
-  // [1] Client was PLAYING_AS 'A', end game, TELL 'B'
-  // [2] Client was PLAYING_AS 'B', end game, TELL 'A'
-  // [3] Client was not playing, do nothing
+    // On disconnect, 
+    // [1] Client was PLAYING_AS 'A' and waiting for 'B', end game
+    // [1] Client was PLAYING_AS 'A', end game, TELL 'B'
+    // [2] Client was PLAYING_AS 'B', end game, TELL 'A'
+    // [3] Client was not playing, do nothing
 
-  if (ws === PLAYER_A) {
-    PLAYER_A = null;
-    if (PLAYER_B != null) {
-      PLAYER_B.send(JSON.stringify({ type: 'opponent-disconnected' }));
-      // Also kick out the other player
-      PLAYER_B = null;
+    if (ws === PLAYER_A) {
+        PLAYER_A = null;
+        if (PLAYER_B != null) {
+            PLAYER_B.send(JSON.stringify({ type: 'opponent-disconnected' }));
+            // Also kick out the other player
+            PLAYER_B = null;
+        }
+        // Start a new game
+        game = FlagsGame(TMP_N, TMP_M, TMP_R);
     }
-    // Start a new game
-    game = FlagsGame(TMP_N, TMP_M, TMP_R);
-  }
-  else if (ws === PLAYER_B) {
-    PLAYER_B = null;
-    if (PLAYER_A != null) {
-      PLAYER_A.send(JSON.stringify({ type: 'opponent-disconnected' }));
-      // Also kick out the other player
-      PLAYER_A = null;
+    else if (ws === PLAYER_B) {
+        PLAYER_B = null;
+        if (PLAYER_A != null) {
+            PLAYER_A.send(JSON.stringify({ type: 'opponent-disconnected' }));
+            // Also kick out the other player
+            PLAYER_A = null;
+        }
+        // Start a new game
+        game = FlagsGame(TMP_N, TMP_M, TMP_R);
     }
-    // Start a new game
-    game = FlagsGame(TMP_N, TMP_M, TMP_R);
-  }
-  else {
-    // notify a waiting player
-    // or broadcast to all waiting players
-  }
+    else {
+        // notify a waiting player
+        // or broadcast to all waiting players
+    }
 
-  clearInterval(ping_interval);
-  console.log('server stopped hearing from client');
-  // Will need again later...
-  // broadcast_online_count();
+    clearInterval(ping_interval);
+    console.log('server stopped hearing from client');
+    // Will need again later...
+    // broadcast_online_count();
 }
 
 function broadcast_online_count() {
-  const online = wss.clients.size;
-  broadcast({ type: 'online', online });
+    const online = wss.clients.size;
+    broadcast({ type: 'online', online });
 }
 
 // The WebSocket server closes
 function shutdown_server() {
-  clear_interval(ping_interval)
+    clear_interval(ping_interval)
 }
 
 function broadcast(msg) {
@@ -147,40 +147,40 @@ function broadcast(msg) {
 }
 
 function receive(socket, msg) {
-  // Ignore requests from non-players
-  if (socket != PLAYER_A && socket != PLAYER_B) {
-    console.log('Request from non-player: ', JSON.stringify(msg));
-    return;
-  }
+    // Ignore requests from non-players
+    if (socket != PLAYER_A && socket != PLAYER_B) {
+        console.log('Request from non-player: ', JSON.stringify(msg));
+        return;
+    }
 
-  console.log(JSON.stringify(msg));
-  switch (msg.type) {
-    case 'select': console.log('select'); handlers.select(socket, msg); break;
-    default: ; // do something...
-  }
+    console.log(JSON.stringify(msg));
+    switch (msg.type) {
+        case 'select': console.log('select'); handlers.select(socket, msg); break;
+        default: ; // do something...
+    }
 }
 
 const handlers = {
-  'select':
-  function(socket, msg) {
-    const gamestate = game.getstate();
+    'select':
+    function(socket, msg) {
+        const gamestate = game.getstate();
 
-    const A_selects = (socket === PLAYER_A && gamestate.turn === 0);
-    const B_selects = (socket === PLAYER_B && gamestate.turn === 1);
+        const A_selects = (socket === PLAYER_A && gamestate.turn === 0);
+        const B_selects = (socket === PLAYER_B && gamestate.turn === 1);
 
-    if (A_selects || B_selects) {
-      var response = game.select(msg.i, msg.j);
-      response.type = 'reveal';
-      response.for = {i: msg.i, j: msg.j};
-      PLAYER_A.send( JSON.stringify(response) );
-      PLAYER_B.send( JSON.stringify(response) );
-    }
-    else {
-      // Player selects out of turn
-      // Do nothing
-      // Client should avoid this situation
-      console.log('Player selects out of turn');
-    }
-  },
+        if (A_selects || B_selects) {
+            var response = game.select(msg.i, msg.j);
+            response.type = 'reveal';
+            response.for = {i: msg.i, j: msg.j};
+            PLAYER_A.send( JSON.stringify(response) );
+            PLAYER_B.send( JSON.stringify(response) );
+        }
+        else {
+            // Player selects out of turn
+            // Do nothing
+            // Client should avoid this situation
+            console.log('Player selects out of turn');
+        }
+    },
 };
 
