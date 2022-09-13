c0055a6c247a50c781b6a0eba821f976186d24c3
marinm <marinmema@live.ca>
2022-09-13 01:55:38 -0400

    Group view elements together

--- browser/page.js
+++ browser/page.js
@@ -25,8 +25,24 @@ const {
     PLAYER_FLAGS,
 } = config;
 
-const notebox  = NoteBox($, '#note-box');
-const scorebox = ScoreBox($, ['#player-0-score', '#player-1-score']);
+
+
+const view = {
+    notebox:
+    NoteBox($, '#note-box'),
+
+    canvasboard:
+    new GameboardCanvas(
+        BOARD_NUM_ROWS,
+        BOARD_NUM_COLUMNS,
+        BOARD_CELL_SIZE,
+        tilesheet,
+    ),
+
+    scorebox:
+    ScoreBox($, ['#player-0-score', '#player-1-score'])
+};
+
 
 const controls = {
     autoplay  : false,
@@ -39,13 +55,7 @@ const gamestate = {
     winner    : null,
 };
 
-// Set up the board
-const gameboardCanvas = new GameboardCanvas(
-    BOARD_NUM_ROWS,
-    BOARD_NUM_COLUMNS,
-    BOARD_CELL_SIZE,
-    tilesheet,
-);
+
 
 
 //
@@ -61,19 +71,19 @@ const socket = QuickWebSocket({
 
 // Add on-click event listener to the canvas
 const boardClicks = clickableCells({
-    element : gameboardCanvas.canvas,
+    element : view.canvasboard.canvas,
     w       : BOARD_CELL_SIZE,
     h       : BOARD_CELL_SIZE,
     onclick : cellOnClick,
-    context : { gamestate, gameboardCanvas, socket },
+    context : { gamestate, canvasboard: view.canvasboard, socket },
 });
 
 // This is not necessary if an error event is also fired on fail
-if (!socket) showStatus('disconnected', notebox, gameboardCanvas, boardClicks);
+if (!socket) showStatus('disconnected', view.notebox, view.canvasboard, boardClicks);
 
 
 function onError() {
-    showStatus('disconnected', notebox, gameboardCanvas, boardClicks);
+    showStatus('disconnected', view.notebox, view.canvasboard, boardClicks);
 }
 
 function onOpen() {
@@ -81,7 +91,7 @@ function onOpen() {
 }
 
 function onClose() {
-    showStatus('disconnected', notebox, gameboardCanvas, boardClicks);
+    showStatus('disconnected', view.notebox, view.canvasboard, boardClicks);
 }
 
 function onMessage(quicksocket, message) {
@@ -89,9 +99,9 @@ function onMessage(quicksocket, message) {
         message,
         $,
         controls,
-        notebox,
-        scorebox,
-        gameboardCanvas,
+        view.notebox,
+        view.scorebox,
+        view.canvasboard,
         gamestate,
         quicksocket,
         boardClicks
@@ -101,15 +111,15 @@ function onMessage(quicksocket, message) {
 
 //------------------------------------------------------------------------------
 
-$('#board-container').append(gameboardCanvas.canvas);
+$('#board-container').append(view.canvasboard.canvas);
 
 $('.remaining').text(' / ' + WINNING_SCORE);
 
 
 document.addEventListener("keyup", function(event) {
     switch (event.keyCode) {
-        case 65: toggleAutoplay(controls, autoplay, $, gamestate, gameboardCanvas, socket);  break; /* a */
-        case 71: autoplay.solverscan(gamestate, gameboardCanvas);                 break; /* g */
+        case 65: toggleAutoplay(controls, autoplay, $, gamestate, view.canvasboard, socket);  break; /* a */
+        case 71: autoplay.solverscan(gamestate, view.canvasboard);                 break; /* g */
         case 78: autoplay.select_next_unrevealed_flag(gamestate, socket);         break; /* n */
     }
 });
