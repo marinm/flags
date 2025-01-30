aab8394769b75723b8f88e81508c41166b439ef5
Marin <marinmema@live.ca>
2025-01-30 18:55:09 -0500

    useRef

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -1,6 +1,5 @@
 import { useState, useEffect, useRef } from "react";
 import { Cell } from "./subcomponents/Cell/Cell";
-import { CellState } from "../../types/CellState";
 import { Location } from "../../types/Location";
 import "./board.css";
 import BoardState from "../../game/BoardState";
@@ -10,17 +9,9 @@ const N_COLS = 25;
 
 export default function Board() {
 	const boardState = useRef(new BoardState(N_ROWS, N_COLS));
-	const [cells, setCells] = useState<CellState[]>(
-		boardState.current.matrix.nodes
-	);
 
 	function onClick(l: Location): void {
 		boardState.current.select(l);
-		setCells([...boardState.current.matrix.nodes]);
-	}
-
-	function index(l: Location): number {
-		return l.i * N_COLS + l.j;
 	}
 
 	return (
@@ -30,7 +21,7 @@ export default function Board() {
 		>
 			{boardState.current.matrix.locations.map((l: Location) => (
 				<div onClick={() => onClick(l)} key={`${l.i}-${l.j}`}>
-					<Cell location={l} state={cells[index(l)]} />
+					<Cell location={l} state={boardState.current.at(l)} />
 				</div>
 			))}
 		</div>
