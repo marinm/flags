d26097f2fc247f7d3297dfba813f0d310a07cf7c
Marin <marinmema@live.ca>
2025-01-30 20:56:01 -0500

    pass BoardState to Board

--- src/components/Game/Game.tsx
+++ src/components/Game/Game.tsx
@@ -1,5 +1,12 @@
+import { useRef } from "react";
 import Board from "./subcomponents/Board/Board";
+import BoardState from "../../game/BoardState";
+
+const N_ROWS = 25;
+const N_COLS = 25;
 
 export default function Game() {
-	return (<Board />);
+	const boardState = useRef(new BoardState(N_ROWS, N_COLS));
+
+	return <Board boardState={boardState.current} />;
 }
--- src/components/Game/subcomponents/Board/Board.tsx
+++ src/components/Game/subcomponents/Board/Board.tsx
@@ -1,28 +1,24 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect } from "react";
 import { CellState } from "../../../../types/CellState";
 import { Cell } from "./subcomponents/Cell/Cell";
 import { Location } from "../../../../types/Location";
 import "./board.css";
 import BoardState from "../../../../game/BoardState";
 
-const N_ROWS = 25;
-const N_COLS = 25;
+export default function Board({ boardState }: { boardState: BoardState }) {
+	const [cells, setCells] = useState<CellState[]>(boardState.all());
 
-export default function Board() {
-	const boardState = useRef(new BoardState(N_ROWS, N_COLS));
-	const [cells, setCells] = useState<CellState[]>(boardState.current.all());
-
-	useEffect(() => setCells([...boardState.current.all()]), []);
+	useEffect(() => setCells([...boardState.all()]), []);
 
 	function onClick(l: Location): void {
-		boardState.current.select(l);
-		setCells([...boardState.current.all()]);
+		boardState.select(l);
+		setCells([...boardState.all()]);
 	}
 
 	return (
 		<div
 			className="board"
-			style={{ gridTemplateColumns: `repeat(${N_COLS}, 1fr)` }}
+			style={{ gridTemplateColumns: `repeat(${boardState.m}, 1fr)` }}
 		>
 			{cells.map((c) => (
 				<div
